89f27b3ade995d831e6be9e37b6b14e136af3ba9
wilsq <viljami.j.viinikainen@gmail.com>
2026-02-19 10:23:48 +0200

    Minor security fix aws_vpc

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 # VPC
+#tfsec:ignore:aws-ec2-require-vpc-flow-logs-for-all-vpcs - Flow logs not required for this learning environment
 resource "aws_vpc" "this" {
-  #tfsec:ignore:aws-ec2-require-vpc-flow-logs-for-all-vpcs - Flow logs not required for this learning environment
+
   cidr_block = "10.0.0.0/16"
 
   tags = {
