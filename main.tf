89c2a50e02c17c76c12231b72aeeac27d6db7098
wilsq <viljami.j.viinikainen@gmail.com>
2026-02-19 10:07:51 +0200

    terraform fmt

--- main.tf
+++ main.tf
@@ -74,9 +74,9 @@ resource "aws_security_group" "web_sg" {
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
     #tfsec:ignore:aws-ec2-no-public-ingress-sgr - Open for learning/testing purposes
     cidr_blocks = ["0.0.0.0/0"]
   }
